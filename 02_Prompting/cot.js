c16d8e35243b5eddbddccaf1923f7498d8461484
Angshuman09 <angshumankashyap69yap69@gmail.com>
2025-08-25 23:07:22 +0530

    cot done

--- 02_Prompting/cot.js
+++ 02_Prompting/cot.js
@@ -1,9 +1,9 @@
-import OpenAI from "openai";
-import "dotenv/config";
+import 'dotenv/config';
+import { OpenAI } from 'openai';
 
 const client = new OpenAI({
-  apiKey: process.env.GEMINI_API_KEY,
-  baseURL: "https://generativelanguage.googleapis.com/v1beta/openai/",
+   baseURL: 'https://openrouter.ai/api/v1',
+  apiKey: process.env.OPENROUTER_API_KEY,
 });
 
 async function main() {
@@ -28,68 +28,83 @@ async function main() {
     User: Can you solve 3 + 4 * 10 - 4 * 3
     ASSISTANT: { "step": "START", "content": "The user wants me to solve 3 + 4 * 10 - 4 * 3 maths problem" } 
     ASSISTANT: { "step": "THINK", "content": "This is typical math problem where we use BODMAS formula for calculation" } 
+    ASSISTANT: { "step": "EVALUATE", "content": "Alright, Going good" } 
     ASSISTANT: { "step": "THINK", "content": "Lets breakdown the problem step by step" } 
-    ASSISTANT: { "step": "THINK", "content": "As per bodmas, first lets solve all multiplications and divisions" } 
+    ASSISTANT: { "step": "EVALUATE", "content": "Alright, Going good" } 
+    ASSISTANT: { "step": "THINK", "content": "As per bodmas, first lets solve all multiplications and divisions" }
+    ASSISTANT: { "step": "EVALUATE", "content": "Alright, Going good" }  
     ASSISTANT: { "step": "THINK", "content": "So, first we need to solve 4 * 10 that is 40" } 
+    ASSISTANT: { "step": "EVALUATE", "content": "Alright, Going good" } 
     ASSISTANT: { "step": "THINK", "content": "Great, now the equation looks like 3 + 40 - 4 * 3" }
+    ASSISTANT: { "step": "EVALUATE", "content": "Alright, Going good" } 
     ASSISTANT: { "step": "THINK", "content": "Now, I can see one more multiplication to be done that is 4 * 3 = 12" } 
+    ASSISTANT: { "step": "EVALUATE", "content": "Alright, Going good" } 
     ASSISTANT: { "step": "THINK", "content": "Great, now the equation looks like 3 + 40 - 12" } 
+    ASSISTANT: { "step": "EVALUATE", "content": "Alright, Going good" } 
     ASSISTANT: { "step": "THINK", "content": "As we have done all multiplications lets do the add and subtract" } 
+    ASSISTANT: { "step": "EVALUATE", "content": "Alright, Going good" } 
     ASSISTANT: { "step": "THINK", "content": "so, 3 + 40 = 43" } 
+    ASSISTANT: { "step": "EVALUATE", "content": "Alright, Going good" } 
     ASSISTANT: { "step": "THINK", "content": "new equations look like 43 - 12 which is 31" } 
-    ASSISTANT: { "step": "THINK", "content": "great, all steps are done and final result is 31" } 
+    ASSISTANT: { "step": "EVALUATE", "content": "Alright, Going good" } 
+    ASSISTANT: { "step": "THINK", "content": "great, all steps are done and final result is 31" }
+    ASSISTANT: { "step": "EVALUATE", "content": "Alright, Going good" }  
     ASSISTANT: { "step": "OUTPUT", "content": "3 + 4 * 10 - 4 * 3 = 31" } 
   `;
 
   const messages = [
     {
-      role: "system",
+      role: 'system',
       content: SYSTEM_PROMPT,
     },
     {
-      role: "user",
-      content: "Write a code in JS to find a prime number as fast as possible",
+      role: 'user',
+      content: 'Write a code in JS to find a prime number as fast as possible',
     },
   ];
 
   while (true) {
     const response = await client.chat.completions.create({
-      model: "gemini-2.0-flash",
+      model: 'openai/gpt-4o-mini',
       messages: messages,
     });
 
     const rawContent = response.choices[0].message.content;
-    const cleanContent = rawContent
-      .replace(/```json\s*/g, "")
-      .replace(/```\s*/g, "")
-      .trim();
-    const parsedContent = JSON.parse(cleanContent);
+    const parsedContent = JSON.parse(rawContent);
 
     messages.push({
-      role: "assistant",
+      role: 'assistant',
       content: JSON.stringify(parsedContent),
     });
 
-    if (parsedContent.step === "START") {
+    if (parsedContent.step === 'START') {
       console.log(`🔥`, parsedContent.content);
       continue;
     }
 
-    if (parsedContent.step === "THINK") {
+    if (parsedContent.step === 'THINK') {
       console.log(`\t🧠`, parsedContent.content);
 
       // Todo: Send the messages as history to maybe gemini and ask for a review and append it to history
       // LLM as a judge techniuqe
+      messages.push({
+        role: 'developer',
+        content: JSON.stringify({
+          step: 'EVALUATE',
+          content: 'Nice, You are going on correct path',
+        }),
+      });
+
       continue;
     }
 
-    if (parsedContent.step === "OUTPUT") {
+    if (parsedContent.step === 'OUTPUT') {
       console.log(`🤖`, parsedContent.content);
       break;
     }
   }
 
-  console.log("Done...");
+  console.log('Done...');
 }
 
 main();
